83ffbb57018eba06ba7267b95412717fb003922b
matthew quinn <matthewquinn.personal@gmail.com>
2017-10-03 13:48:18 -0400

    refactored

--- src/test/java/com/matthewquinn/LargePizzaTest.java
+++ src/test/java/com/matthewquinn/LargePizzaTest.java
@@ -2,19 +2,25 @@ package com.matthewquinn;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LargePizzaTest {
 
-    @Test
-    public void shouldCost12NinetyNineForLargePizza() {
-        Pizza pizza = new LargePizza();
-        Assert.assertEquals(pizza.cost(),12.99,2);
+    private Pizza pizza;
+
+    @Before
+    public void setUpLargePizza() throws Exception {
+        pizza = new LargePizza();
     }
 
     @Test
     public void shouldReturnLargePizzaDescription() {
-        Pizza pizza = new LargePizza();
         Assert.assertEquals(pizza.getDescription(),"Large Pizza");
+    }
+
+    @Test
+    public void shouldCost12NinetyNineForLargePizza() {
+        Assert.assertEquals(pizza.cost(),12.99,2);
     }
 }
