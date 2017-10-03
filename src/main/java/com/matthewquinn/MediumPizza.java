634e78d160dfa56e58d2badfd8ac3a5b77dd8eca
matthew quinn <matthewquinn.personal@gmail.com>
2017-10-03 14:10:16 -0400

    wrote medium pizza descrip

--- src/main/java/com/matthewquinn/MediumPizza.java
+++ src/main/java/com/matthewquinn/MediumPizza.java
@@ -3,6 +3,10 @@ package com.matthewquinn;
 
 public class MediumPizza extends Pizza {
 
+    public MediumPizza() {
+        description = "Medium Pizza";
+    }
+
     @Override
     public double cost() {
         return 0;
