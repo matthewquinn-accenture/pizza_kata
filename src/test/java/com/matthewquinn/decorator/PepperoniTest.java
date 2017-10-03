6a766baa9fe6cf0485e24f5833314272217dda18
matthew quinn <matthewquinn.personal@gmail.com>
2017-10-03 14:33:59 -0400

    refactored

--- src/test/java/com/matthewquinn/decorator/PepperoniTest.java
+++ src/test/java/com/matthewquinn/decorator/PepperoniTest.java
@@ -2,19 +2,25 @@ package com.matthewquinn.decorator;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PepperoniTest {
 
+    private Topping topping;
+
+    @Before
+    public void setUpTopping() throws Exception {
+        topping = new Pepperoni();
+    }
+
     @Test
     public void shouldReturnPepperoniDescription() {
-        Topping topping = new Pepperoni();
         Assert.assertEquals(topping.getDescription(), "Pepperoni");
     }
 
     @Test
     public void shouldCost29CentsForPepperoniTopping() {
-        Topping topping = new Pepperoni();
         Assert.assertEquals(topping.cost(), .29,-2);
     }
 }
