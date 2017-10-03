ce92383a426f362b8f2d3931e531141267d58702
matthew quinn <matthewquinn.personal@gmail.com>
2017-10-03 14:03:57 -0400

    wrote description for small pizza

--- src/main/java/com/matthewquinn/SmallPizza.java
+++ src/main/java/com/matthewquinn/SmallPizza.java
@@ -3,6 +3,10 @@ package com.matthewquinn;
 
 public class SmallPizza extends Pizza {
 
+    public SmallPizza() {
+        description = "Small Pizza";
+    }
+
     @Override
     public double cost() {
         return 0;
