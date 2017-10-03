97dcc3a9d9332c65a59c30ee1425a18f222efa3a
matthew quinn <matthewquinn.personal@gmail.com>
2017-10-03 13:33:57 -0400

    wrote test for large Pizza

---
+++ src/test/java/com/matthewquinn/LargePizzaTest.java
@@ -0,0 +1,14 @@
+package com.matthewquinn;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LargePizzaTest {
+
+    @Test
+    public void shouldCost12NinetyNineForLargePizza() {
+        Pizza pizza = new LargePizza();
+        Assert.assertEquals(pizza.cost(),12.99,2);
+    }
+}
